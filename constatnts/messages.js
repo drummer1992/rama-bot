add661be700c9a9e55a71360b4dec996744df0ce
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-02-21 18:22:10 +0200

    - updated /about description

--- constatnts/messages.js
+++ constatnts/messages.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const { SET_GROUP, ADD_TRAINING, INFO } = require('./routes')
-const { HAPPY, ROBO } = require('./emoji')
+const { SET_GROUP, ADD_TRAINING, INFO, FORCE } = require('./routes')
+const { HAPPY, ROBO, PLUS } = require('./emoji')
 
 exports.ABOUT_MESSAGE = `Привіт ${HAPPY}, я Rama Vandama Bot ${ROBO}, ось список моїх, доступних команд:\n\n`
   + `${SET_GROUP} (назва групи) - встановити групу в якій ви займаєтеся (надалі, групу можна змінювати)\n`
   + `${ADD_TRAINING} (назва групи) (години:хвилини) - якщо Ви тренер, цією командою можна створити тренування вказавши групу та час\n`
   + `${INFO} - подивитися інформацію про наступне тренування\n`
+  + `${PLUS} - записатися на тренування\n`
+  + `${FORCE} - записатися навіть якщо тренування не у вашої групи\n`
