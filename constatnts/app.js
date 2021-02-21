06def32276a5247e735714bb53cac63b32b8303c
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-02-22 01:01:21 +0200

    - quick fix

--- constatnts/app.js
+++ constatnts/app.js
@@ -1,11 +1,11 @@
 'use strict'
 
 const SET_GROUP = exports.SET_GROUP = '/setgroup'
+const CREATE_TRAINING = exports.CREATE_TRAINING = '/createtraining'
 
 exports.HELP = '/help'
 exports.START = '/start'
 exports.CREATE_GROUP = '/creategroup'
-exports.CREATE_TRAINING = '/createtraining'
 exports.INFO = '/info'
 exports.FORCE = '/force'
 
